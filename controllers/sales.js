708112f1847d3c1c274dcac80732a093ce83cb0a
Ignacio Rodriguez <inrodriguez@e3stores.com>
2022-06-30 01:02:59 -0300

    Ignacio Rodriguez

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,7 +1,39 @@
 const sales = require('../data/supplies');
 
-async function getSales(){    
+async function getAllSales(){    
     return sales.getAllSales();
 }
 
-module.exports = {getSales};
+async function getSalesById(id){
+    return sales.getSalesById(id)
+}
+
+async function SalesByPurchaseMethod(purchase){
+    console.log(purchase)
+    let allSales = await sales.getAllSales()
+    let salesFilter = allSales.filter(s => s.purchaseMethod === purchase)
+
+    return salesFilter
+}
+
+async function getSalesByEmail(email){
+    let allSales = await sales.getAllSales()
+    let salesByEmail = allSales.find(m => m.customer.email == email)
+    console.log(salesByEmail)
+    let map = {
+        email: salesByEmail.customer.email,
+        item: salesByEmail.item.toArray()
+    }
+    console.log(map)
+
+    return map
+}
+
+async function getSalesSatisfaction(){
+    let allSales = await sales.getAllSales();
+    let satisfaction = allSales.filter(m => m.customer.satisfaction < 3)
+    
+    return satisfaction
+}
+
+module.exports = {getAllSales, getSalesById, SalesByPurchaseMethod, getSalesByEmail, getSalesSatisfaction};
--- routes/sales.js
+++ routes/sales.js
@@ -4,7 +4,23 @@ const controller = require('../controllers/sales');
 
 router.get('/', async (req, res) => {
     console.log("check");
-    res.json(await controller.getSales());
+    res.json(await controller.getAllSales());
+});
+
+router.get('/getSalesById/:id', async (req, res) => {  
+    res.json(await controller.getSalesById(req.params.id));
+});
+
+router.get('/SalesByPurchaseMethod/:purchaseMethod', async (req, res) => { 
+    res.json(await controller.SalesByPurchaseMethod(req.params.purchaseMethod));
+});
+
+router.get('/getSalesByEmail/:email', async (req, res) => { 
+    res.json(await controller.getSalesByEmail(req.params.email));
+});
+
+router.get('/getSalesSatisfaction', async (req, res) => { 
+    res.json(await controller.getSalesSatisfaction());
 });
 
 module.exports = router;
